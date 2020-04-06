ef67248edfb2e6a816cded884cfcc38dd09b7983
vanzantom <54019885+vanzantom@users.noreply.github.com>
2020-04-06 14:12:05 +0200

    Add files via upload

--- GDGMatlab/1D/example_FEM.m
+++ GDGMatlab/1D/example_FEM.m
@@ -1,23 +1,38 @@
-%% Example script to use the 1D FEM code
-clear all;
-a=0;% geometry
-b=1;
-f=@(x) -exp(x)*(cos(x)-2*sin(x)-x*cos(x)-x*sin(x)); %force term
-c=@(x) exp(x); % diffusion term
-Dirichlet_fun=@(x) (1-x)*0 + x*cos(1) ; % boundary condition. Label Dirichlet-1
-Neumann_fun=@(x) cos(1)-sin(1); %Label Dirichlet-2
+%-------------------------------------------------------------------------
+% example_FEM is an example of script using Poisson_solver_1D function to solve
+% a Poisson problem on the interval (a,b) with Dirichlet/Neumann BC with P1
+% or P2 FE spaces.
+% Further, it checks the order of convergence.
+
+% author: Tommaso Vanzan
+%-------------------------------------------------------------------------
+
+clear all; close all;
+% The structure geo contains the extrema of the interval Omega=(a,b)
+geo.a=0;
+geo.b=1;
+% The structure data contains information about the data of the problem,
+% e.g. force term, diffusion coefficient, boundary conditions
+data.f=@(x) -exp(x)*(cos(x)-2*sin(x)-x*cos(x)-x*sin(x)); 
+data.c=@(x) exp(x); % diffusion term
+data.Dirichlet_fun=@(x) (1-x)*0 + x*cos(1) ; % boundary condition. % Label Dirichlet=-1
+data.Neumann_fun=@(x) cos(1)-sin(1); %Label Neumann=-2
+data.left=-1;%label variable for left node. If data.left=-1, then we have Dirichlet B.C. on x=a. If data.left=-1, then we have Neumann B.C. on x=a
+data.right=-1;%label variable for right node. 
+% Variables for Postprocessing
 uex=@(x) x.*cos(x);%exact solution
 plt=0;%plot variable
 hh=[1/4;1/8;1/16;1/32;1/64;1/128];%array of mesh size
-%=======convergence plot
-%Linear
+%=================================
+%   Linear
+%=================================
 basis_type=101;% Linear P1
 for i=1:length(hh)
-h=hh(i);%mesh size
-u=Poisson_solver_1D(a,b,h,basis_type,c,f,-1,-1,Dirichlet_fun,Neumann_fun);%solve the Problem
-x=(0:h:1)';
+geo.h=hh(i);%mesh size
+u=Poisson_solver_1D(geo,basis_type,data);%solve the Problem
+x=(0:geo.h:1)';
 uexvec=uex(x);%evaluate exact solution
-normvec(i)=norm(uexvec-u,2)*sqrt(h);
+normvec(i)=norm(uexvec-u,2)*sqrt(geo.h);
 if plt==1
     plot(x,u)%plot approximation
     hold on
@@ -29,14 +44,16 @@ end
 loglog(hh,normvec,'b*-')
 hold on
 loglog(hh,hh.^2,'r')
-% Quadratic
+%=================================
+%   Quadratic
+%=================================
 basis_type=102;%Quadratic Finite element
 for i=1:length(hh)
-h=hh(i);%mesh size
-u2order=Poisson_solver_1D(a,b,h,basis_type,c,f,-1,-1,Dirichlet_fun,Neumann_fun);%solve problem
-x2order=(0:h/2:1)';
+geo.h=hh(i);%mesh size
+u2order=Poisson_solver_1D(geo,basis_type,data);%solve problem
+x2order=(0:geo.h/2:1)';
 uexvec=uex(x2order);
-normvec(i)=norm(uexvec-u2order,2)*sqrt(h);
+normvec(i)=norm(uexvec-u2order,2)*sqrt(geo.h);
 if plt==1
     plot(x2order,u2order)
     hold on
@@ -51,5 +68,5 @@ loglog(hh,hh.^4,'m')
 grid on
 xlabel('h')
 ylabel('errors')
-legend({'$$\|u_h-u\|_{L^2}-P1$$','$$h^2$$','$$\|u_h-u\|_{L^2}-P2$$','$$h^4}$$'},'interpreter','latex')
+legend({'$$\|u_h-u\|_{L^2}-P1$$','$$h^2$$','$$\|u_h-u\|_{L^2}-P2$$','$$h^{4}$$'},'interpreter','latex')
 set(gca,'FontSize',12);
--- GDGMatlab/1D/example_IP.m
+++ GDGMatlab/1D/example_IP.m
@@ -1,24 +1,41 @@
-%% Example script to use the 1D DG Symmetric internal penalty method
+%-------------------------------------------------------------------------
+% example_IP is an example of script using Poisson_solver_1DIP() function to solve
+% a Poisson problem on the interval (a,b) with Dirichlet/Neumann BC with
+% linear or quadratic DG spaces using Interior penalty method.
+% Further, it checks the order of convergence.
+
+% Attention: Neumann boundary condition not implemented jet!
+
+% author: Tommaso Vanzan
+%-------------------------------------------------------------------------
 clear all;
-a=0;% geometry
-b=1;
-f=@(x) 2*pi*exp(x).*(2*pi*sin(2*pi*x)-cos(2*pi*x)); %force term
-c=@(x) exp(x); % diffusion term
-Dirichlet_fun=@(x) 0 ; % boundary condition
-Neumann_fun=@(x) cos(1)-sin(1); %DO NOT USE NEUMANN
+% The structure geo contains the extrema of the interval Omega=(a,b).
+geo.a=0;
+geo.b=1;
+% The structure data contains information about the data of the problem,
+% e.g. force term, diffusion coefficient, boundary conditions
+data.f=@(x) 2*pi*exp(x).*(2*pi*sin(2*pi*x)-cos(2*pi*x)); %force term
+data.c=@(x) exp(x); % diffusion term
+data.Dirichlet_fun=@(x) 0 ; % boundary condition
+data.Neumann_fun=@(x) cos(1)-sin(1); %DO NOT USE NEUMANN
+data.left=-1;%label variable for left node. If data.left=-1, then we have Dirichlet B.C. on x=a. If data.left=-1, then we have Neumann B.C. on x=a
+data.right=-1;%label variable for right node. 
+% Penalization Constant
+alpha=8; 
+% Variables for Postprocessing
 uex=@(x) sin(2*pi*x);
 uex_der=@(x) 2*pi*cos(2*pi*x);
 hh=[1/4;1/8;1/16;1/32;1/64;1/128];
-alpha=8; %costant for penalization
 plt=0;
-%=======convergence plot
-%Linear
+%=================================
+%   Linear DG Interior Penalty
+%=================================
 basis_type=101;
 for i=1:length(hh)
-h=hh(i); %mesh size
-penalty=alpha/h; %penalty term 
-[u,P,T,Pb,Tb]=Poisson_solver_1DIP(a,b,h,basis_type,c,f,-1,-1,Dirichlet_fun,Neumann_fun,penalty);
-x=(0:h:1)'; %discrete mesh
+geo.h=hh(i); %mesh size
+penalty=alpha/geo.h; %penalty term 
+[u,P,T,Pb,Tb]=Poisson_solver_1DIP(geo,basis_type,data,penalty);
+x=(0:geo.h:1)'; %discrete mesh
 uexvec=uex(x); %comput exact solution
 if plt==1
     figure(1); hold on;
@@ -38,13 +55,15 @@ figure(2)
 loglog(hh,err,'b*-')
 hold on
 loglog(hh,hh.^1,'r')
-% Quadratic
+%=================================
+%   Quadratic DG Interior Penalty
+%=================================
 basis_type=102;
 for i=1:length(hh)
-h=hh(i);
-penalty=alpha/h;
-[u2order,P,T,Pb,Tb]=Poisson_solver_1DIP(a,b,h,basis_type,c,f,-1,-1,Dirichlet_fun,Neumann_fun,penalty);
-x2order=(0:h/2:1)'; %discrete mesh
+geo.h=hh(i);
+penalty=alpha/geo.h;
+[u2order,P,T,Pb,Tb]=Poisson_solver_1DIP(geo,basis_type,data,penalty);
+x2order=(0:geo.h/2:1)'; %discrete mesh
 uexvec=uex(x2order); %exact solution
 if plt==1
     figure(1); hold on;
