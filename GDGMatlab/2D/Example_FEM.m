c59e10949d6f1a9df50c46654dbb9f91f11194b1
vanzantom <54019885+vanzantom@users.noreply.github.com>
2020-05-04 15:24:11 +0200

    Add files via upload

--- GDGMatlab/2D/Example_FEM.m
+++ GDGMatlab/2D/Example_FEM.m
@@ -1,6 +1,6 @@
 %-------------------------------------------------------------------------
 % example_FEM is an example of script using Poisson_solver_2D function to solve
-% a Poisson problem on the geometry specified with Dirichlet BC on the boundary and P1 FE
+% a Poisson problem on the geometry specified with Dirichlet BC on the boundary and P1 or P2 FE
 % space.
 % Further, it checks the order of convergence.
 
@@ -23,11 +23,11 @@ data.Dirichlet_fun=@(x,y) 0; % boundary condition
 % The structure para contains parameters of the problem such as the order
 % of Gauss quadrature.
 para.order_Gauss=2; %Order Gauss quadrature.
-para.basis_type=201; %P1 DG space
+para.basis_type=201; %P1 FE space 201, P2 FE space 202
 
 % Variables for Postprocessing
 uex=@(x,y) sin(pi*x).*sin(pi*y); % Exact solution
-hh=[1/4;1/8;1/16;1/32;1/64]; % Vector of mesh sizes
+hh=[1/2;1/8;1/16;1/32;1/64]; % Vector of mesh sizes
 plt=0; % Set equal to 0 to disable plot, equal to 1 to plot solution
 %=================================
 %   Check Convergence
@@ -35,28 +35,29 @@ plt=0; % Set equal to 0 to disable plot, equal to 1 to plot solution
 
 for i=1:length(hh)
 geo.h=hh(i);
-[P,E,T,u]=Poisson_solver_2D_FEM(geo,data,para);%Solve Poisson Problem
-for k=1:size(P,2)%evaluate exact solution on the mesh nodes.
-	x=P(1,k); y=P(2,k);
+[P,E,T,Pb,Tb,Eb,u]=Poisson_solver_2D_FEM(geo,data,para);%Solve Poisson Problem
+for k=1:size(Pb,2)%evaluate exact solution on the mesh nodes.
+	x=Pb(1,k); y=Pb(2,k);
 	uexvec(k)=uex(x,y);
 end
 [error_L2(i),error_H1(i)]=compute_errors(u,uexvec,P,T,T((1:3),:),E,para,'FEM');%Compute L2 and H1 errors.
 if plt==1
     figure(1)
-    pdemesh(P,E,T,u); % Plot the numerical solution.
+    pdemesh(Pb,Eb,Tb,u); % Plot the numerical solution.
 %   figure(2)
 %   pdemesh(P,E,T,u, 'mesh', 'on');% Plot the mesh
 %   pdemesh(P,E,T,uexvec,); % Plot the exact solution.
     pause
 end
 end
-loglog(hh,error_L2,'b*-')
+loglog(hh,hh,'m')
 hold on
 loglog(hh,hh.^2,'r')
+loglog(hh,hh.^3,'c')
+loglog(hh,error_L2,'b*-')
 loglog(hh,error_H1,'k*-')
-loglog(hh,hh,'m')
 grid on
 xlabel('h')
 ylabel('errors')
-legend({'$$\|u_h-u\|_{L^2}$$','$$h^{2}$$','$$\|u_h-u\|_{H_1}$$','$$h$$'},'interpreter','latex')
+legend({'$$h$$','$$h^{2}$$','$$h^{3}$$','$$\|u_h-u\|_{L^2}$$','$$\|u_h-u\|_{H_1}$$',},'interpreter','latex')
 set(gca,'FontSize',12);
